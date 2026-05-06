582cdb9bda677e39c5acf5b7f20a307178c7da69
Ayushman Tiwari <ayushman28at@gmail.com>
2026-05-06 22:49:03 +0530

    Add files via upload

---
+++ tpecasting.py
@@ -0,0 +1,22 @@
+# a = "1"
+# b = "2"
+
+# print(int(a) + int(b))
+
+# EXPLICIT CONVERSION
+
+'''
+string = "15"
+number = 7
+string_number = int(string)
+sum = number + string_number
+print("The sum of both number is: ",sum)
+'''
+
+
+
+# IMPLICIT CONVERSION  
+
+c= 1.9
+d = 8
+print(c + d)
